84041990eb31a5297cb25e63de1edc2ab887d2f3
bigsaltyfishes <bigsaltyfishes@gmail.com>
2023-10-17 11:39:12 +0800

    Project: Update target framework to net7.0-windows
    * Also enable AOT
    
    Signed-off-by: bigsaltyfishes <bigsaltyfishes@gmail.com>

--- ServerProxy/Program.cs
+++ ServerProxy/Program.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace ServerProxy;
 
 internal static class Program
@@ -10,6 +12,7 @@ internal static class Program
     {
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
+        ComWrappers.RegisterForMarshalling(WinFormsComInterop.WinFormsComWrappers.Instance);
         ApplicationConfiguration.Initialize();
         Application.Run(new Form1());
     }
