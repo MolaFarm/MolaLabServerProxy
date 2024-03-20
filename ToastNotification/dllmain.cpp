b455e83686c1c68581457dadfc07550cf7542604
bigsaltyfishes <bigsaltyfishes@gmail.com>
2024-03-20 10:26:00 +0800

    ToastNotification: Fix potential exception when init multiple times
    Signed-off-by: bigsaltyfishes <bigsaltyfishes@gmail.com>

--- ToastNotification/dllmain.cpp
+++ ToastNotification/dllmain.cpp
@@ -6,11 +6,20 @@ using namespace winrt;
 using namespace Windows::Data::Xml::Dom;
 using namespace Windows::UI::Notifications;
 
+static bool is_init = false;
+static ToastNotifier* manager;
+
 extern "C" __declspec(dllexport) void Show(const char* appname, const char* title, const char* message)
 {
-    init_apartment();
+    if (!is_init)
+    {
+        init_apartment();
+        static auto new_manager = ToastNotificationManager::CreateToastNotifier(to_hstring(appname));
+        manager = &new_manager;
+        is_init = true;
+    }
 
-    auto manager = ToastNotificationManager::CreateToastNotifier(to_hstring(appname));
+    //static ToastNotifier manager = ToastNotificationManager::CreateToastNotifier(to_hstring(appname));
     auto toastXml = ToastNotificationManager::GetTemplateContent(ToastTemplateType::ToastText02);
 
     auto elements = toastXml.GetElementsByTagName(L"text");
@@ -18,5 +27,5 @@ extern "C" __declspec(dllexport) void Show(const char* appname, const char* titl
     elements.Item(1).InnerText(to_hstring(message));
 
     auto toast = ToastNotification(toastXml);
-    manager.Show(toast);
+    manager->Show(toast);
 }
